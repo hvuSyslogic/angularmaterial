ea1833b17a6defaeaf4ac4d168384bb5b9608337
Nicolet Fear4 <vu@nicoletfear.com>
2021-03-14 22:20:24 -0500

    add route to the demo module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './shared/material.module';
+import { DemoModule } from './demo/demo.module';
 
+const routes: Routes = [
+  { path: 'demo', loadChildren: async () => (await import('./demo/demo.module') ).DemoModule },
+  { path: '**', redirectTo: 'demo' }
+];
 
 @NgModule({
   declarations: [
@@ -13,7 +18,7 @@ import { MaterialModule } from './shared/material.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/demo/demo.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MaterialModule } from '../shared/material.module';
+import { FormsModule } from '@angular/forms';
+
+import { DemoRoutingModule } from './demo-routing.module';
+import { ButtonsComponent } from './buttons/buttons.component';
+
+
+@NgModule({
+  declarations: [ButtonsComponent],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    DemoRoutingModule,
+
+  ]
+})
+export class DemoModule { }
